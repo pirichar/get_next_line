9cc782b7a53e61d874dd349f721959a00473d13c
Pier-luc Richard <pirichar@c3r12p7.42quebec.com>
2021-10-06 14:43:41 -0400

    remove fd condition

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pirichar <pirichar@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 08:28:10 by pirichar          #+#    #+#             */
-/*   Updated: 2021/10/06 14:07:21 by pirichar         ###   ########.fr       */
+/*   Updated: 2021/10/06 14:42:53 by pirichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,8 @@ char	*next_line(int fd, int ret, char *buf, char *saved)
 	if (ret < BUFFER_SIZE)
 	{
 		ret = read(fd, buf, BUFFER_SIZE);
+		if (ret == -1)
+			return NULL;	
 		buf[ret] = '\0';
 		tmp = ft_strjoin(saved, buf);
 		free (saved);
@@ -74,24 +76,21 @@ char	*get_next_line(int fd)
 	int			ret;
 	char		buf[BUFFER_SIZE + 1];
 	int			i;
-
-	if (fd >= 0)
-	{
+	if (!saved)
+	{	
+		saved = malloc(sizeof(char) * BUFFER_SIZE + 1);
 		if (!saved)
-		{
-			saved = malloc(sizeof(char) * BUFFER_SIZE + 1);
-			if (!saved)
-				return (NULL);
-		}
-		ret = 1;
-		i = 0;
-		while (nl_find(saved) == 0)
-			saved = next_line(fd, ret, buf, saved);
-		while (saved[i] && saved[i] != '\n')
-			i++;
-		line = ft_substr(saved, 0, i + 1);
-		saved = free_stuff(saved, i);
-		return (line);
+			return (NULL);
 	}
-	return (NULL);
+	ret = 1;
+	i = 0;
+	while (nl_find(saved) == 0)
+		saved = next_line(fd, ret, buf, saved);
+	if (!saved)
+		return NULL;
+	while (saved[i] && saved[i] != '\n')
+		i++;
+	line = ft_substr(saved, 0, i + 1);
+	saved = free_stuff(saved, i);
+	return (line);
 }
