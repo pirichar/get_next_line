d8048679798bea2500ebe805b1b4e2897426113f
Pier-luc Richard <pirichar@c3r12p7.42quebec.com>
2021-10-06 17:46:19 -0400

    same stuff as last commit but with master

---
+++ main.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pirichar <pirichar@student.42quebec>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 08:27:28 by pirichar          #+#    #+#             */
+/*   Updated: 2021/10/06 17:32:08 by pirichar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "get_next_line.h"
+#include <fcntl.h>
+
+int main()
+{
+	int fd;
+	int fd2;
+	char *test;
+//	char *test2;
+	int i;
+	int x;
+
+	i = 0;
+	x = 7;
+	fd = open("no_new_line", O_RDONLY);
+	if (fd == -1)
+		return (1);
+	fd2 = open("test2", O_RDONLY);
+	if (fd == -1)
+		return (1);
+
+	test = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if(test == NULL)
+		return (1);	
+	//	printf("FROM MAIN : Printing first fd %i times = \n", x);
+	while(i < x)
+	{
+	test = get_next_line(fd);
+	printf("FROM MAIN : %s", test);
+	i++;
+	free (test);
+	}
+}
