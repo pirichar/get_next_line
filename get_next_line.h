e6533ae05a3e163554130e818c3f6fbe8e16fb04
Pier-luc Richard <pirichar@c1r3p9.42quebec.com>
2021-10-12 09:13:52 -0400

    fresh start

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: pirichar <pirichar@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 09:13:52 by pirichar          #+#    #+#             */
-/*   Updated: 2021/10/06 09:37:40 by pirichar         ###   ########.fr       */
+/*   Updated: 2021/10/12 09:11:53 by pirichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # include <stdio.h>
 # include <limits.h>
 
-char	*ft_strjoin(char *s1, const char *s2);
+/*char	*ft_strjoin(char *s1, const char *s2);
 size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
@@ -27,6 +27,6 @@ char	*ft_substr(const char *s, unsigned int start, size_t len);
 
 int		nl_find(char *str);
 char	*next_line(int fd, char *buf, char *saved);
-char	*get_next_line(int fd);
+char	*get_next_line(int fd);*/
 
 #endif
