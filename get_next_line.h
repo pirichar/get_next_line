96cb7acca8fbccd8c6a74ae9dc28f71e23123df2
pirichar <pirichar@student.42quebec.com>
2021-10-14 16:19:19 -0400

    woops forgot to update my .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: pirichar <pirichar@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 09:13:52 by pirichar          #+#    #+#             */
-/*   Updated: 2021/10/12 11:48:09 by pirichar         ###   ########.fr       */
+/*   Updated: 2021/10/14 16:19:02 by pirichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*get_next_line(int fd);
 char	*free_stuff(char *saved, char *new_line);
 char	*ft_strchr(const char *s, int c);
+char	*read_file(int fd, char *saved, char **new_line);
 
 
 #endif
