93818cddba455bd32838d00f9915064d88c55273
pirichar <pirichar@student.42quebec.com>
2021-10-21 10:40:13 -0400

    moulinetted and everything

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: pirichar <pirichar@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 09:13:52 by pirichar          #+#    #+#             */
-/*   Updated: 2021/10/20 11:32:07 by pirichar         ###   ########.fr       */
+/*   Updated: 2021/10/21 10:39:30 by pirichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 char	*ft_strjoin_free(char *s1, const char *s2);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 
-char	*get_next_line_bonus(int fd);
+char	*get_next_line(int fd);
 char	*free_stuff(char *saved, char *new_line);
 char	*ft_strchr(const char *s, int c);
 char	*read_file(int fd, char *saved, char **new_line);
